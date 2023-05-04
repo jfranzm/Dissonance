6fd1748f5165d41b0c25dc9d6055ade38d43b9b9
Jeffrey Martinez <martinez.jeffreyfranz@gmail.com>
2023-05-03 22:26:24 -0400

    create FriendList component

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,10 @@
+import './FriendList.css'
+
+export default function FriendList() {
+    return (
+    <div className="friend-bar">
+        <h3 className='friend-title'>Friends</h3>
+
+    </div>
+    )
+}
--- src/pages/MainHubPage/MainHubPage.jsx
+++ src/pages/MainHubPage/MainHubPage.jsx
@@ -1,12 +1,10 @@
 import './MainHubPage.css'
+import FriendList from '../../components/FriendList/FriendList'
 
 export default function MainHubPage() {
     return (
         <main className="MainHubPage">
-            <div className="friend-bar">
-                <h3 className='friend-title'>Friends</h3>
-
-            </div>
+            <FriendList />
             <div className="content-container">
                 <div className="fav-list">
                     <h3 className='sect-name'>Favourites</h3>
